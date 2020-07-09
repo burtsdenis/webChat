c2520099d4bac1fdbbed558ea46370073ed71e15
denis.burtsev <denis.burtsev@thinfilmnfc.com>
2020-07-09 18:17:21 +0300

    some refactoring

--- src/main/java/ru/geekbrains/webchat/Message.java
+++ src/main/java/ru/geekbrains/webchat/Message.java
@@ -1,7 +1,11 @@
 package ru.geekbrains.webchat;
 
 
+import java.text.SimpleDateFormat;
+
 public class Message {
+
+    private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private final String message;
     private final String author;
     private final long date;
@@ -25,4 +29,8 @@ public class Message {
         return date;
     }
 
+    @Override
+    public String toString() {
+        return format.format(this.date) + " " + this.getAuthor() + ": " + this.getMessage() + "\n";
+    }
 }
--- src/main/java/ru/geekbrains/webchat/gui/ClientGUI.java
+++ src/main/java/ru/geekbrains/webchat/gui/ClientGUI.java
@@ -12,7 +12,7 @@ import java.text.SimpleDateFormat;
 
 public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
 
-    private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
 
 
     private static final int WIDTH = 400;
@@ -128,7 +128,7 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
     }
 
     private void drawMessage(Message message) {
-        chatArea.append(format.format(message.getDate()) + " " + message.getAuthor() + ": " + message.getMessage() + "\n");
+        chatArea.append(message.toString());
         logMessage(message);
     }
 
@@ -137,7 +137,7 @@ public class ClientGUI extends JFrame implements ActionListener, Thread.Uncaught
         try (FileWriter fw = new FileWriter(logDir + "/chatLog.txt", true);
              BufferedWriter bw = new BufferedWriter(fw)) {
 
-            bw.write(format.format(message.getDate()) + " " + message.getAuthor() + ": " + message.getMessage());
+            bw.write(message.toString());
             bw.newLine();
         } catch (IOException e) {
             System.out.println("Cannot log message, logFile does not exist or cannot access to logsFolder");
